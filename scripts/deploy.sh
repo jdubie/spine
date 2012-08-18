ca2a8591cb478e5b4cfd8c19d3f5aeec4c388433
Jack Dubie <jack@orchestra.com>
2012-08-18 13:44:51 -0700

    Added rsync to deploy file

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,6 +1,4 @@
 #!/bin/sh
 
 node_modules/.bin/cake build
-mkdir ../porcupine/node_modules/spine -p
-cp -r lib ../porcupine/node_modules/spine/lib
-cp -r src ../porcupine/node_modules/spine/src
+rsync . ../porcupine/node_modules/spine -r
