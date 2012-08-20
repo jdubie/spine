17d92c9a7dc528a368efe24d3551ec068775ebc7
Jack Dubie <jack@orchestra.com>
2012-08-19 17:05:59 -0700

    Fixed deploy script to overwrite vendor/spine in porky

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,3 +2,5 @@
 
 node_modules/.bin/cake build
 rsync . ../porcupine/node_modules/spine -r
+cp -r lib ../porcupine/vendor/scripts/spine
+mv ../porcupine/vendor/scripts/spine/lib/spine.js ../porcupine/vendor/scripts/spine/spine.js
